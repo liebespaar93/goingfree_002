6f997321ad7bc569d67b6ee419e86c8cce3e6092
liebespaar93 <liebespaar93@gmail.com>
2023-09-02 15:24:40 +0900

    [Fix] side-bar motion close update

--- docs/js/header/header.js
+++ docs/js/header/header.js
@@ -1,4 +1,5 @@
 
 function sidebar_toggle() {
     document.getElementById("sidebar").classList.toggle('sidebar-none');
+    document.getElementById("main").classList.toggle('main-blur');
 }
---
+++ docs/js/tools/event-ctr.js
@@ -0,0 +1,9 @@
+
+/**
+ * 버븦필 캡쳐링 제어
+ * @link https://inpa.tistory.com/entry/JS-%F0%9F%93%9A-%EB%B2%84%EB%B8%94%EB%A7%81-%EC%BA%A1%EC%B3%90%EB%A7%81
+ */
+function stop_propagation(e)
+{
+    e.stopPropagation();
+}
